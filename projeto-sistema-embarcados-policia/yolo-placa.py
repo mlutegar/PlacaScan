9b07e534ed9ae438218d485ab3d695065c3b8eb9
Michel Lutegar <mlutegar@gmail.com>
2025-04-15 21:01:56 -0300

    feat: adicionando projeto

--- projeto-sistema-embarcados-policia/yolo-placa.py
+++ projeto-sistema-embarcados-policia/yolo-placa.py
@@ -1,7 +1,21 @@
 import os
+import re
+
 import cv2
 import pytesseract
 from ultralytics import YOLO
+
+
+def valida_texto(text):
+    if len(text) < 7:  # Verifica se o comprimento total é ao menos 7 (4 letras + 3 números)
+        return False
+    if len(text) > 10:
+        return False
+    if len(re.findall(r'[a-zA-Z]', text)) < 4:  # Verifica se há pelo menos 4 letras
+        return False
+    if len(re.findall(r'\d', text)) < 3:  # Verifica se há pelo menos 3 números
+        return False
+    return True
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -12,7 +26,10 @@ os.makedirs(save_dir, exist_ok=True)
 # Carrega modelo
 model = YOLO('placa-veicular-model.pt')
 
-cap = cv2.VideoCapture(0)
+# Use 0 para webcam ou 'video.mp4' para arquivo de vídeo
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('rua1.mp4')
+
 counter = 0
 
 while True:
@@ -30,6 +47,10 @@ while True:
         # Configurações: PSM 7 (uma linha), whitelist só A–Z e 0–9
         config = '--oem 3 --psm 7 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         text = pytesseract.image_to_string(crop, config=config).strip()
+
+        if not valida_texto(text):
+            continue
+
         print(f"[{counter:04d}] Placa reconhecida:", text)
 
         # Salva imagem e TXT
